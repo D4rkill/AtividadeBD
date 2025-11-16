c64e35d4c6eefaf075206431d6d9d197b0a3c6fc
William <williamoliveira20040@gmail.com>
2025-11-15 22:06:00 -0300

    Commit final para Envio da Atividade

--- README.md
+++ README.md
@@ -8,15 +8,51 @@ Método 1 = public Connection conectarBD()
 
 Método 2 = public boolean verificarUsuario(String login, String senha)
 
+________________________________________________________________________
 
+Método 1:
 
 Complexidade Ciclomática do Método 1: 
 
 M = E - N + 2P
 
-M = 6
+E = 6
 N = 6
 P = 1
 
-Cálculo: 
+Cálculo: M = 6 - 6 + 2 * 1
+Cálculo: M = 2
+
+A complexidade ciclomática M desse código é 2.
+
+Caminhos Possíveis: 
+1 - 2 - 3 - 6;
+1 - 4 - 5 - 6;
+
+Total de 2 Caminhos possíveis
+
+________________________________________________________________________
+
+Método 2:
+
+Complexidade Ciclomática do Método 1: 
+
+M = E - N + 2P
+
+E = 9
+N = 8
+P = 1
+
+Cálculo: M = 9 - 8 + 2 * 1
+Cálculo: 3
+
+A complexidade ciclomática M desse código é 3.
+
+Caminhos Possíveis: 
+1 - 2 - 3 - 4 - 5 - 8;
+1 - 2 - 3 - 8;
+1 - 6 - 7 - 8;
+
+Total de 3 Caminhos possíveis
+
 
--- User.java
+++ User.java
@@ -6,56 +6,52 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-    // Método 1 
+    // Método 1 - Realiza a conexão com o banco de dados
     public Connection conectarBD() {
-        // Nó 1
+        // Nó 1 - Declara a variável de conexão
         Connection conn = null;
+        // Nó 2 - Tentativa de abrir a conexão
         try {
-            // Nó 2
+            // Nó 3 - Carrega o driver do MySQL e cria a conexão
             Class.forName("com.mysql.Driver.Manager").newInstance();
-            // Nó 3
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-            // Nó 4
             conn = DriverManager.getConnection(url);
-        } catch (Exception e) {
-            // Nó 5
+        } // Nó 4 - Disparado ao ocorrer erro na conexão
+        catch (Exception e) {
+            // Nó 5 - Tratamento de erro
         }
-        // Nó 6
+        // Nó 6 - Retorna a conexão
         return conn;
     }
 
     public String nome = "";
     public boolean result = false;
-    
-    // Método 2
+
+    // Método 2 - Verifica se o usuário existe no banco de dados
     public boolean verificarUsuario(String login, String senha) {
-        // Nó 1
+        // Nó 1 - Monta o comando SQL e conecta ao banco
         String sql = "";
-        // Nó 2
         Connection conn = conectarBD();
-        // Instrução SQL
-        // Nó 3
+        // Estrutura da consulta do SQL
         sql += "select nome from usuarios ";
-        // Nó 4
         sql += "where login = " + "'" + login + "'";
-        // Nó 5
         sql += " and senha = " + "'" + senha + "'";
+        // Nó 2 - Tenta executar a consulta
         try {
-            // Nó 6
+            // Nó 3 - Cria o Statement e executa o SQL
             Statement st = conn.createStatement();
-            // Nó 7
             ResultSet rs = st.executeQuery(sql);
-            // Nó 8
+            // Nó 4 - Verifica se encontrou algum registro
             if (rs.next()) {
-                // Nó 9
+                // Nó 5 - Salva nome e ajusta o resultado
                 result = true;
-                // Nó 10
                 nome = rs.getString("nome");
             }
-        } catch (Exception e) {
-            // Nó 11
+        } // Nó 6 - Disparado ao ocorrer erro na consulta
+        catch (Exception e) {
+            // Nó 7 - Tratamento de erro
         }
-        // Nó 12
+        // Nó 8 - Retorna se o usuário foi encontrado
         return result;
     }
 }
